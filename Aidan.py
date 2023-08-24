ba8789cca670be3a55541f6da5a5428aea69d967
Adan banuelos <banuelosaidan3@gmail.com>
2023-08-24 18:01:24 +0000

    commited

--- Aidan.py
+++ Aidan.py
@@ -1,1 +1,3 @@
-git add Aidan.py
+distance = int(input("enter the distance in light years."))
+Speed = int(input("enter the spacecraft speed in mph."))
+print(distance* 670616629/Speed, "hours is the time it would take to travel", distance, "light years at", Speed, "mph")
